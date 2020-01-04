42b1e4b1b3961485c8b7e61133d70d2372e9aa8e
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-01-03 23:57:11 -0300

    Initial Commit

---
+++ SWoL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWoL
+//
+//  Created by Pedro Giuliano Farina on 03/01/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
