d405570c98ccc6dc5d44c6eb37d066729f51a998
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-03-30 22:01:32 -0300

    Adding animation

--- Swol/Controller/TutorialViewController.swift
+++ Swol/Controller/TutorialViewController.swift
@@ -16,4 +16,58 @@ public class TutorialViewController: UIViewController {
     @IBAction func endTutorialTap(_ sender: Any?) {
         self.dismiss(animated: true, completion: nil)
     }
+
+    @IBOutlet var computer: UIImageView?
+    public override func viewDidLoad() {
+        _ = TutorialViewController.wakingImages
+        _ = TutorialViewController.sleepingImages
+    }
+
+    public override func viewWillAppear(_ animated: Bool){
+        computer?.image = TutorialViewController.sleepingImages[0]
+        computer?.animationImages = TutorialViewController.sleepingImages
+        computer?.animationDuration = 1.2
+        computer?.animationRepeatCount = Int.max
+        computer?.startAnimating()
+    }
+
+    @IBAction func continueTapWithAnimation(_ sender: Any) {
+        self.computer?.stopAnimating()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.computer?.image = TutorialViewController.wakingImages[7]
+            self.computer?.animationImages = TutorialViewController.wakingImages
+            self.computer?.animationDuration = 0.8
+            self.computer?.animationRepeatCount = 1
+            self.computer?.startAnimating()
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+            self.performSegue(withIdentifier: "continue", sender: self)
+        }
+    }
+
+    private static var sleepingImages: [UIImage] = {
+        var images:[UIImage] = []
+
+        for i in 0...8 {
+            if let image = UIImage(named: "Sleeping0\(i)") {
+                images.append(image)
+            }
+        }
+
+        return images
+    }()
+
+    private static var wakingImages: [UIImage] = {
+        var images: [UIImage] = []
+
+        for i in 0...8 {
+            if let image = UIImage(named: "WakingUp0\(i)") {
+                images.append(image)
+            }
+        }
+
+        return images
+    }()
 }
