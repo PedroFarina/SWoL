e9491f64192813b3dc469944a782174543ce2b6d
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-04-03 17:33:07 -0300

    Adding ads (#6)
    * Adding banner ad
    
    * Adding rewarded ads
    
    * Adding localizables
    
    * Adding private keys
    
    * Adding interstitial ads

---
+++ Swol/Controller/Devices/AddingDeviceTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  AddingDeviceTableViewController.swift
+//  SWoL
+//
+//  Created by Pedro Giuliano Farina on 04/01/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+import SwolBackEnd
+
+public class AddingDeviceTableViewController: UITableViewController {
+
+    public var mockTint: UIColor = {
+        if #available(iOS 13, *) {
+            return UIColor { (UITraitCollection: UITraitCollection) -> UIColor in
+                if UITraitCollection.userInterfaceStyle == .dark {
+                    /// Return the color for Dark Mode
+                    return #colorLiteral(red: 0.1098039216, green: 0.1098039216, blue: 0.1176470588, alpha: 1)
+                } else {
+                    /// Return the color for Light Mode
+                    return #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+                }
+            }
+        } else {
+            /// Return a fallback color for iOS 12 and lower.
+            return #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+        }
+    }()
+
+    @IBOutlet weak var nameTableViewCell: TextTableViewCell!
+    @IBOutlet weak var ipTableViewCell: TextTableViewCell!
+    @IBOutlet weak var macTableViewCell: TextTableViewCell!
+    @IBOutlet weak var portTableViewCell: TextTableViewCell!
+    @IBOutlet weak var btnDone: UIBarButtonItem!
+    @IBOutlet weak var footerTableViewCell: UITableViewCell!
+    var footerText: String = "footer0".localized()
+
+    public var device: DeviceProtocol?
+
+    public override func viewDidLoad() {
+        if let device = device {
+            self.navigationItem.title = "Editing".localized()
+            btnDone.title = "Save".localized()
+            btnDone.isEnabled = true
+            nameTableViewCell.txtText = device.name
+            ipTableViewCell.txtText = device.address
+            macTableViewCell.txtText = device.mac
+            portTableViewCell.txtText = String(device.port)
+            footerText = "footer7".localized()
+        }
+        ipTableViewCell.changedCharacters = IPChanged(_:)
+        macTableViewCell.changedCharacters = checkDone(_:)
+        portTableViewCell.changedCharacters = checkDone(_:)
+        footerTableViewCell.textLabel?.text = footerText
+        footerTableViewCell.accessibilityLabel = footerText
+        footerTableViewCell.accessibilityNavigationStyle = .combined
+        footerTableViewCell.backgroundColor = mockTint
+    }
+
+    private var currentIP: String? = nil
+    private func IPChanged(_ text:String?) {
+        if let txt = text,
+            let old = currentIP{
+            if txt.count > old.count
+                && (txt.count == 3 || txt.count == 7 || txt.count == 11) {
+                ipTableViewCell.txtText = txt + "."
+            }
+        }
+        currentIP = text
+        checkDone(text)
+    }
+
+    private func checkDone(_ text: String?) {
+        var footerNum = 0
+        if !(ipTableViewCell.txtText?.isEmpty ?? true) {
+            footerNum += 1
+        }
+        if !(macTableViewCell.txtText?.isEmpty ?? true) {
+            footerNum += 2
+        }
+        if !(portTableViewCell.txtText?.isEmpty ?? true) {
+            footerNum += 4
+        }
+        footerText = "footer\(footerNum)".localized()
+        if footerNum == 7 {
+            if Int32(portTableViewCell.txtText ?? "") != nil {
+                btnDone.isEnabled = true
+            } else {
+                footerText = "footer8".localized()
+            }
+        } else {
+            btnDone.isEnabled = false
+        }
+        footerTableViewCell.textLabel?.tintColor = .lightGray
+        footerTableViewCell.textLabel?.text = footerText
+        footerTableViewCell.accessibilityLabel = footerText
+    }
+
+    @IBAction func doneTap(_ sender: UIBarButtonItem) {
+        guard let name = nameTableViewCell.txtText,
+            let ip = ipTableViewCell.txtText,
+            var mac = macTableViewCell.txtText,
+            let portString = portTableViewCell.txtText,
+            let port = Int32(portString) else {
+                return
+        }
+        mac = mac.replacingOccurrences(of: "-", with: ":").uppercased()
+        do {
+            if let device = device {
+                try DataManager.shared(with: iCloudAccessManager.permission).editDevice(device, newName: name, newAddress: ip, newMacAddress: mac, newPort: port)
+            } else {
+                try DataManager.shared(with: iCloudAccessManager.permission).registerDevice(name: name, address: ip, macAddress: mac, port: port)
+            }
+            self.dismiss(animated: true)
+        } catch let err {
+            footerTableViewCell.textLabel?.tintColor = .red
+            footerTableViewCell.textLabel?.text = err.localizedDescription
+            UIAccessibility.post(notification: .announcement, argument: footerTableViewCell)
+        }
+    }
+    @IBAction func cancelTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
---
+++ Swol/Controller/Devices/DevicesTableViewController.swift
@@ -0,0 +1,154 @@
+//
+//  DevicesTableViewController.swift
+//  SWoL
+//
+//  Created by Pedro Giuliano Farina on 04/01/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+import SwolBackEnd
+
+public class DevicesTableViewController: UITableViewController, DataWatcher {
+
+    public private(set) var devices: [DeviceProtocol] = DataManager.shared(with: iCloudAccessManager.permission).devices
+    public private(set) var selectedDevice: DeviceProtocol?
+
+    public override func viewDidLoad() {
+        tableView.tableFooterView = UIView()
+        navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    public override func viewWillDisappear(_ animated: Bool) {
+        DataManager.shared(with: iCloudAccessManager.permission).removeAsWatcher(self)
+    }
+    public override func viewWillAppear(_ animated: Bool) {
+        DataManager.shared(with: iCloudAccessManager.permission).addAsWatcher(self)
+        updateData()
+    }
+
+    public func dataUpdated() {
+        updateData()
+    }
+
+    private func updateData() {
+        devices = DataManager.shared(with: iCloudAccessManager.permission).devices
+        tableView.reloadData()
+    }
+
+
+    public override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devices.isEmpty ? 1 : devices.count
+    }
+
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if devices.isEmpty {
+            let cell = UITableViewCell()
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textAlignment = .center
+            cell.textLabel?.text = "You have no devices yet.\nPlease tap on '+' to create one.".localized()
+            cell.isUserInteractionEnabled = false
+            return cell
+        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "deviceCell") else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = devices[indexPath.row].name
+        cell.detailTextLabel?.text = devices[indexPath.row].mac
+        cell.isUserInteractionEnabled = true
+        return cell
+    }
+
+    override public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return !devices.isEmpty
+    }
+
+    public override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        let delete = UIContextualAction(style: .destructive, title: "Delete".localized()) { (_, _, success) in
+            DispatchQueue.main.async {
+                self.deleteAction(on: self.devices[indexPath.row])
+                success(true)
+            }
+        }
+
+        let edit = UIContextualAction(style: .normal, title: "Edit".localized()) { (_, _, success) in
+            DispatchQueue.main.async {
+                self.editAction(on: self.devices[indexPath.row])
+                success(true)
+            }
+        }
+        edit.backgroundColor = .systemOrange
+
+        let config = UISwipeActionsConfiguration(actions: [delete, edit])
+        config.performsFirstActionWithFullSwipe = true
+        return config
+    }
+
+    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.isEditing {
+            editAction(on: devices[indexPath.row])
+        } else {
+            wakeAction(on: devices[indexPath.row])
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    private func deleteAction(on device: DeviceProtocol) {
+        let cont = UIAlertController(title: "Deletion confirmation".localized(), message: "Do you want to delete ".localized() + (device.name ?? "John".localized()), preferredStyle: .alert)
+        let yes = UIAlertAction(title: "Yes".localized(), style: .destructive) { (_) in
+            do {
+                try DataManager.shared(with: iCloudAccessManager.permission).removeDevice(device)
+            } catch let err {
+                DispatchQueue.main.async {
+                    let error = UIAlertController(title: "Error!".localized(), message: err.localizedDescription, preferredStyle: .alert)
+                    error.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.parent?.present(error, animated: true)
+                }
+            }
+        }
+        let no = UIAlertAction(title: "No".localized(), style: .cancel, handler: nil)
+        cont.addAction(yes)
+        cont.addAction(no)
+        self.parent?.present(cont, animated: true)
+    }
+
+    private func editAction(on device:  DeviceProtocol) {
+        selectedDevice = device
+        self.parent?.performSegue(withIdentifier: "newDevice", sender: self)
+        selectedDevice = nil
+    }
+
+    private func wakeAction(on device: DeviceProtocol) {
+        let cont = UIAlertController(title: "Waking confirmation".localized(), message: "Do you want to wake ".localized() + (device.name ?? "John") + "?", preferredStyle: .alert)
+        let yes = UIAlertAction(title: "Yes".localized(), style: .default) { (_) in
+            let okAction = UIAlertAction(title: "Ok", style: .default) { (_) in
+                let ad = AdManager.createAndLoadInterstitialAd()
+                if let parent = self.parent, ad.isReady {
+                    ad.present(fromRootViewController: parent)
+                }
+            }
+            if let err = Awake.target(device: device) {
+                DispatchQueue.main.async {
+                    let error = UIAlertController(title: "Error!".localized(), message: err.localizedDescription, preferredStyle: .alert)
+                    error.addAction(okAction)
+                    self.parent?.present(error, animated: true)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    let success = UIAlertController(title: "Success".localized(), message: "Packet sent to ".localized() + (device.getBroadcast() ?? ""), preferredStyle: .alert)
+                    success.addAction(okAction)
+                    self.parent?.present(success, animated: true)
+                }
+            }
+        }
+        let no = UIAlertAction(title: "No".localized(), style: .cancel, handler: nil)
+        cont.addAction(yes)
+        cont.addAction(no)
+        self.parent?.present(cont, animated: true)
+    }
+}
