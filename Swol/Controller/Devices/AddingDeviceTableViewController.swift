86a56768ff1798b95682108b3ddac189e50516c7
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-12-06 14:46:27 -0300

    Removing "." auto complete

--- Swol/Controller/Devices/AddingDeviceTableViewController.swift
+++ Swol/Controller/Devices/AddingDeviceTableViewController.swift
@@ -49,26 +49,13 @@ public class AddingDeviceTableViewController: UITableViewController {
             portTableViewCell.txtText = String(device.port)
             footerText = "footer7".localized()
         }
-        ipTableViewCell.changedCharacters = IPChanged(_:)
+        ipTableViewCell.changedCharacters = checkDone(_:)
         macTableViewCell.changedCharacters = checkDone(_:)
         portTableViewCell.changedCharacters = checkDone(_:)
         footerTableViewCell.textLabel?.text = footerText
         footerTableViewCell.accessibilityLabel = footerText
         footerTableViewCell.accessibilityNavigationStyle = .combined
         footerTableViewCell.backgroundColor = mockTint
-    }
-
-    private var currentIP: String? = nil
-    private func IPChanged(_ text:String?) {
-        if let txt = text,
-            let old = currentIP{
-            if txt.count > old.count
-                && (txt.count == 3 || txt.count == 7 || txt.count == 11) {
-                ipTableViewCell.txtText = txt + "."
-            }
-        }
-        currentIP = text
-        checkDone(text)
     }
 
     private func checkDone(_ text: String?) {
